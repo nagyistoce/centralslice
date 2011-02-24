517708ea58c77c0aee1b49552a8607826a644ab9
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-02-24 18:26:12 +0000

    Merge my own version to trunk

--- run.m
+++ run.m
@@ -19,13 +19,29 @@ N=256;
 P=eye(N);
 
 P = phantom('Modified Shepp-Logan',N); 
-%P = zeros(N);
-%P(200,100)=1;
+N=128;
 
+%% Create Phantom P
+%P = phantom('Shepp-Logan',N);
+
+%Modified Shepp-Logan' gives better visual perception than 'Shepp-Logan'
+%P = phantom('Modified Shepp-Logan',N);     %Fail-safe version
+
+% a simple 2D delta function which is off-centre
+%P = zeros(N);
+%P(12,12)=1;
+
+% a straight line
+P=eye(N);
+
+% Determine the range of x and y
+x = linspace(-N/2,N/2,N);
+y = x;
+
+% Show and save the phantom
 figure(1)
-imagesc(P)
-xlabel('x'),ylabel('y'),colormap(gray),colorbar
-title('Shepp-Logan head model')
+imagesc(x,y,P),colormap(gray),colorbar
+title('Phantom'),xlabel('x'),ylabel('y')
 print -dpng 1_phantom.png
 
 %% Apply radon transformation
@@ -38,74 +54,101 @@ theta = 0:1:180;
 %% Apply noise to the Radon image
 %Rf = Rf + SNR * ( rand(s_size,theta_size)*2 - 1 );
 
-%% Show radon image
+%% Apply noise to the Radon image
+%Rf = Rf + SNR * ( rand(s_size,theta_size)*2 - 1 );
 
+
+%% Show and save Radon image
 figure(2)
-r = (256^2 * 2)^0.5;  
-imagesc(0, -r/2, RT);
-xlabel('theta'),ylabel('s'),colormap(gray),colorbar
-title('Radon image')
+imagesc(theta,s,Rf),colormap(gray),colorbar
+title('Radon image'),xlabel('theta'),ylabel('s')
 print -dpng 2_radon.png
 
-%% polar fourier transform
-F2 = fftshift(fft(RT),1);
+%% Polar Fourier transform
+FRf = fftshift(fft(Rf),1);         % Apply 1D Fourier transform in each column
 
-[a,b]=size(F2);
-theta=(1:b);
-omega=(1:a)-floor(a/2);
-[Theta Omega]=meshgrid(theta,omega);
+% Determine the range of omega
+dx=mean(diff(x));
+dy=mean(diff(y));
+ds=sqrt( dx^2 + dy^2 );
+size_omega=size(FRf,1);
+omega_max=2*pi/ds*max(s);
+omega=linspace(-omega_max,omega_max,size_omega);
 
+%% Show and save the Fourier transform image
 figure(3)
-imagesc(theta, omega, real(F2)),colormap(gray),colorbar
+imagesc(theta, omega, real(FRf)),colormap(gray),colorbar
 xlabel('theta'),ylabel('omega_s')
-title('polar fourier transform of Radon image, Re')
+title('Polar Fourier transform of Radon image, Real Part')
 print -dpng 3a_fourier_radon_real.png
 
 figure(4)
-imagesc(theta, omega, imag(F2)),colormap(gray),colorbar
+imagesc(theta, omega, imag(FRf)),colormap(gray),colorbar
 xlabel('theta'),ylabel('omega_s')
-title('polar fourier transform of Radon image, Im')
+title('Polar Fourier transform of Radon image, Imagimary Part')
 print -dpng 3b_fourier_radon_imag.png
 
-%% change to x-y coordinate
-F22 = horzcat(F2,F2,F2);
-theta2=(1-b:b+b);
-omega2=(1:a)-floor(a/2);
-[Theta2 Omega2]=meshgrid(theta2,omega2);
+%% Change from polar to x-y coordinate
+FRf_widescreen = horzcat(FRf,FRf,FRf,FRf);       % Increase the range of theta by four times
 
-xx=(1:N)-N/2;
-yy=(1:N)-N/2;
-[XX YY]=meshgrid( xx , yy );
-th = atan2(YY,XX).*180./pi;
-w = sqrt( XX.^2 + YY.^2 ) .* sign (th);
-th = abs(th);
+%% Determine the new range of theta and omega
+[size_omega size_theta]=size(FRf_widescreen);
+theta_max=max(theta);
+omega_widescreen=omega;
+theta_widescreen=linspace(-360,180+theta_max,size_theta);
+[THETA OMEGA]=meshgrid(theta_widescreen,omega_widescreen);
 
-Fxy = interp2(Theta2,Omega2,F22,th,w);
-Fxy(isnan(Fxy))=0;        % set all nan to zero
+%% DEBUG: show and save the widescreen image
+if(DEBUG)
+figure(98)
+imagesc(theta_widescreen, omega_widescreen, real(FRf_widescreen)),colormap(gray),colorbar
+xlabel('theta'),ylabel('omega_s')
+title('Polar Fourier transform of Radon image, Real Part')
+print -dpng 3c_fourier_radon_real.png
+
+figure(99)
+imagesc(theta_widescreen, omega_widescreen, imag(FRf_widescreen)),colormap(gray),colorbar
+xlabel('theta'),ylabel('omega_s')
+title('Polar Fourier transform of Radon image, Imagimary Part')
+print -dpng 3d_fourier_radon_imag.png
+end
+
+% Determine range of the desired xy scale
+x = linspace(-N/2,N/2,N);
+y = x;
+omega_x_max=2*pi/dx*max(x)/sqrt(2);
+omega_x = linspace(-omega_x_max,omega_x_max,N);
+omega_y = omega_x;
+[WX WY]=meshgrid( omega_x , omega_y );
+xy_to_theta = atan2(WY,WX).*180./pi;
+xy_to_omega = sqrt( WX.^2 + WY.^2 );
+
+F2f = interp2(THETA,OMEGA,FRf_widescreen,xy_to_theta,xy_to_omega);
+F2f(isnan(F2f))=0;        % set all NaN (Not a Number) error to zero
 
 figure(5)
-imagesc(xx,yy,real(Fxy)),colormap(gray),colorbar
+imagesc(x,y,real(F2f)),colormap(gray),colorbar
 xlabel('omega_x'),ylabel('omega_y')
-title('polar to x-y cubic interpolation, Re')
+title('polar to x-y cubic interpolation, Real Part')
 print -dpng 4a_fourier_xy_real.png
 
 figure(6)
-imagesc(xx, yy, imag(Fxy)),colormap(gray),colorbar
+imagesc(x, y, imag(F2f)),colormap(gray),colorbar
 xlabel('omega_x'),ylabel('omega_y')
-title('polar to x-y cubic interpolation, Im')
+title('polar to x-y cubic interpolation, Imaginary Part')
 print -dpng 4b_fourier_xy_imag.png
 
 %% inverse FFT
-f = ifft2(Fxy);
+f = ifft2(fftshift(F2f));
 
 figure(7)
-imagesc(xx,yy,real(f)),colormap(gray),colorbar
+imagesc(x,y,real(f)),colormap(gray),colorbar
 xlabel('x'),ylabel('y')
-title('Inverse FFT, Re')
+title('Reconstructed Image, Real Part')
 print -dpng 5_reconstruct_xy_real.png
 
 figure(8)
-imagesc(xx,yy,imag(f)),colormap(gray),colorbar
+imagesc(x,y,imag(f)),colormap(gray),colorbar
 xlabel('x'),ylabel('y')
-title('Inverse FFT, Im')
+title('Reconstructed Image, Imaginary Part')
 print -dpng 5_reconstruct_xy_imag.png
