624e6d8274072c7ce00f182eedd97beb560d03b7
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-02-24 07:13:13 +0000

    Make sourcecode more compatible to Matlab 2007

--- trunk/run.m
+++ trunk/run.m
@@ -1,78 +1,85 @@
-%Size of reconstructed image
+
+%% Size of reconstructed image
 N=256;
 
+%% Create Phantom
 P = phantom('Modified Shepp-Logan',N);
 %P = zeros(N);
 %P(200,100)=1;
 
 figure(1)
 imagesc(P)
-xlabel("x"),ylabel("y"),colormap(gray),colorbar
+xlabel('x'),ylabel('y'),colormap(gray),colorbar
 print -dpng 1_phantom.png
 
 % apply radon transformation
 [RT,XP] = radon(P);
 
-% noise?
+% apply noise?
+%RT = RT * rand
 
-%Show radon image
+%% Show radon image
 figure(2)
 imagesc(RT);
-xlabel("theta"),ylabel("s"),colormap(gray),colorbar
+xlabel('theta'),ylabel('s'),colormap(gray),colorbar
 print -dpng 2_radon.png
 
-% polar fourier transform
+%% polar fourier transform
 F2 = fftshift(fft(RT),1);
 
-%F21 = vertcat( F2([ceil(a/2):a],:), F2([1:floor(a/2)],:));
+%% F21 = vertcat( F2([ceil(a/2):a],:), F2([1:floor(a/2)],:));
 
 [a,b]=size(F2);
-[Theta Omega]=meshgrid([1:b],[1:a]-floor(a/2));
+theta=(1:b);
+omega=(1:a)-floor(a/2)
+[Theta Omega]=meshgrid(theta,omega);
 
 figure(3)
-imagesc(Theta, Omega, real(F2))
-xlabel("theta"),ylabel("omega_s")
+imagesc(theta, omega, real(F2)),colormap(gray),colorbar
+xlabel('theta'),ylabel('omega_s')
 print -dpng 3a_fourier_radon_real.png
 
 figure(4)
-imagesc(Theta, Omega, imag(F2))
-xlabel("theta"),ylabel("omega_s")
+imagesc(theta, omega, imag(F2)),colormap(gray),colorbar
+xlabel('theta'),ylabel('omega_s')
 print -dpng 3b_fourier_radon_imag.png
 
-%change to x-y coordinate
-
+%% change to x-y coordinate
 F22 = horzcat(F2,F2,F2);
-[Theta2 Omega2]=meshgrid([1-b:b+b],[1:a]-floor(a/2));
+theta2=(1-b:b+b);
+omega2=(1:a)-floor(a/2);
+[Theta2 Omega2]=meshgrid(theta2,omega2);
 
-[XX YY]=meshgrid( [1:N]-N/2 , [1:N]-N/2 );
+xx=(1:N)-N/2;
+yy=(1:N)-N/2;
+[XX YY]=meshgrid( xx , yy );
 th = atan2(YY,XX).*180./pi;
 w = sqrt( XX.^2 + YY.^2 ) .* sign (th);
 th = abs(th);
 
 Fxy = interp2(Theta2,Omega2,F22,th,w);
-%set all nan to zero
+% set all nan to zero
 Fxy(isnan(Fxy))=0;
 
 figure(5)
-imagesc(XX,YY,real(Fxy))
-axis equal,xlabel("omega_x"),ylabel("omega_y")
+imagesc(xx,yy,real(Fxy)),colormap(gray),colorbar
+xlabel('omega_x'),ylabel('omega_y')
 print -dpng 4a_fourier_xy_real.png
 
 figure(6)
-imagesc(XX, YY, imag(Fxy))
-axis equal,xlabel("omega_x"),ylabel("omega_y")
+imagesc(xx, yy, imag(Fxy)),colormap(gray),colorbar
+xlabel('omega_x'),ylabel('omega_y')
 print -dpng 4b_fourier_xy_imag.png
 
-%inverse fft
+%% inverse fft
 f = ifft2(Fxy);
 
 figure(7)
-imagesc(XX,YY,real(f))
-axis equal,xlabel("x"),ylabel("y")
+imagesc(xx,yy,real(f)),colormap(gray),colorbar
+xlabel('x'),ylabel('y')
 print -dpng 5_reconstruct_xy_real.png
 
 figure(8)
-imagesc(XX,YY,imag(f))
-axis equal,xlabel("x"),ylabel("y")
+imagesc(xx,yy,imag(f)),colormap(gray),colorbar
+xlabel('x'),ylabel('y')
 print -dpng 5_reconstruct_xy_imag.png
-
