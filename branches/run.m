5714ec99e791d507ad74c69125f81a5c93953fba
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-02-26 16:02:48 +0000

    Adding for figures for debugging

--- branches/run.m
+++ branches/run.m
@@ -10,33 +10,41 @@ DEBUG=1;
 %% Interpolation method: nearest / linear / cubic
 interp_m='nearest';
 
+%%Zeropadding ratio: filter out high frequency signal
+% 0:none , 1:filter everything
+zeropad_ratio=0;
+
+%%Oversampling ratio: reduce alising
+%1:normal, >1 oversampling
+oversamp_ratio=1;
+
 %% Size of phantom and reconstructed image
 % N is the number of rows and columns in P, which is 256 respectively
 N=64;
 
 %Radon scan angles
 %we may change the number of beams that we use to cover 180 degree
-theta=linspace(0,180-1,180);
+theta=linspace(0,180-1/3,180*3);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Create Phantom P
 %P = phantom('Shepp-Logan',N);
 
 %Modified Shepp-Logan' gives better visual perception than 'Shepp-Logan'
-%P = phantom('Modified Shepp-Logan',N);     %Fail-safe version
+P = phantom('Modified Shepp-Logan',N);     %Fail-safe version
 
 % a simple 2D delta function which is off-centre
 %P = zeros(N);
 %P(12,12)=1;
 
 % a stripe
-%T=6;
+%T=N/8;
 %P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
 
 % a square (T must be even)
-T=8;
-P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
-P=P'.*P; %image of square
+%T=2;
+%P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
+%P=P'&P; %image of square
 
 % a 45deg straight line
 %P=eye(N);
@@ -80,7 +88,7 @@ print -dpng 2_radon.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Polar Fourier transform
-FRf = fftshift(fft(Rf),1);         % Apply 1D Fourier transform in each column
+FRf = fft(Rf);         % Apply 1D Fourier transform in each column
 
 % Determine the range of omega
 ds=mean(diff(s));
@@ -89,19 +97,32 @@ d_omega=2*pi/ds;
 omega_max=size_omega*d_omega/2;
 omega=linspace(-omega_max,omega_max,size_omega);
 
+%% Zeropadding: remove high-frequency components and reduce noise
+pad_begin=floor(size_omega*(1-zeropad_ratio)/2)+1;
+pad_end=size_omega-pad_begin+1;
+FRf(pad_begin:pad_end,:)=0;
+FRf=fftshift(FRf,1);
+
 %% Show and save the Fourier transform image
 figure(3)
+imagesc(theta, omega, abs(FRf)),colormap(gray),colorbar
+xlabel('theta'),ylabel('omega_s')
+title('Polar Fourier transform of Radon image, Absolute Value')
+print -dpng 3_fourier_radon_abs.png
+
+if(DEBUG)
+figure(4)
 imagesc(theta, omega, real(FRf)),colormap(gray),colorbar
 xlabel('theta'),ylabel('omega_s')
 title('Polar Fourier transform of Radon image, Real Part')
 print -dpng 3a_fourier_radon_real.png
 
-figure(4)
+figure(5)
 imagesc(theta, omega, imag(FRf)),colormap(gray),colorbar
 xlabel('theta'),ylabel('omega_s')
 title('Polar Fourier transform of Radon image, Imaginary Part')
 print -dpng 3b_fourier_radon_imag.png
-
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Change from polar to x-y coordinate
 
@@ -114,6 +135,8 @@ label_WY = WW.*sind(THETA);
 omega_x = omega;
 omega_y = omega;
 [WX WY]=meshgrid( omega_x , omega_y );
+
+N=N*oversamp_ratio;
 d_omega=mean(diff(omega));
 dx=2*pi/d_omega;
 max_x=N*dx/2;
@@ -129,18 +152,18 @@ FRf_array=reshape(FRf,cols*rows,1);
 
 %% Apply interpolation
 F2f = griddata(WX_array,WY_array,FRf_array,WX,WY,interp_m);
-%F2f(isnan(F2f))=0;        % set all NaN (Not a Number) error to zero
+F2f(isnan(F2f))=0;        % set all NaN (Not a Number) error to zero
 
 %% DEBUG: test label WX,WY
 if(DEBUG)
-figure(95)
+figure(6)
 surf(label_WX,label_WY,abs(FRf)),colorbar,colormap(jet)
 axis tight;
 title('DEBUG: Radon Fourier image before interpolation, Absolute Value')
 xlabel('theta'),ylabel('omega')
 print -dpng 4c_fourier_polar_abs.png
 
-figure(96)
+figure(7)
 surf(WX,WY,abs(F2f)),colorbar,colormap(jet)
 axis tight;
 title('DEBUG: Radon Fourier image after interpolation, Absolute Value')
@@ -148,31 +171,40 @@ xlabel('omega_x'),ylabel('omega_y')
 print -dpng 4d_fourier_xy_abs.png
 end
 
-%% Save image
-figure(5)
+%% Save image of F2f
+figure(8)
+imagesc(omega_x,omega_y,abs(F2f)),colormap(gray),colorbar
+xlabel('omega_x'),ylabel('omega_y')
+title('polar to x-y interpolation, Absolute Value')
+print -dpng 4_fourier_xy_abs.png
+
+if(DEBUG)
+figure(9)
 imagesc(omega_x,omega_y,real(F2f)),colormap(gray),colorbar
 xlabel('omega_x'),ylabel('omega_y')
 title('polar to x-y interpolation, Real Part')
 print -dpng 4a_fourier_xy_real.png
 
-figure(6)
+figure(10)
 imagesc(omega_x, omega_y, imag(F2f)),colormap(gray),colorbar
 xlabel('omega_x'),ylabel('omega_y')
 title('polar to x-y interpolation, Imaginary Part')
 print -dpng 4b_fourier_xy_imag.png
-
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Apply Inverse FFT to F2f
-f = ifft2(fftshift(F2f));
+f = fftshift(ifft2(fftshift(F2f)));
 
-figure(7)
+figure(11)
 imagesc(x,y,real(f)),colormap(gray),colorbar
 xlabel('x'),ylabel('y')
 title('Reconstructed Image, Real Part')
 print -dpng 5_reconstruct_xy_real.png
 
-figure(8)
+if(DEBUG)
+figure(12)
 imagesc(x,y,imag(f)),colormap(gray),colorbar
 xlabel('x'),ylabel('y')
 title('Reconstructed Image, Imaginary Part')
 print -dpng 5_reconstruct_xy_imag.png
+end
