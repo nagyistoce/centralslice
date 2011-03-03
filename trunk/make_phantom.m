5bfc26ada3a3fe79301958cdd8c515183050c741
beckchang88@gmail.com <beckchang88@gmail.com@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-03 12:28:10 +0000

    & becomes *, to remove -1

--- trunk/make_phantom.m
+++ trunk/make_phantom.m
@@ -22,10 +22,10 @@ switch shape
   case {'dot'}
     T=2;
     P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
-    P=P'&P;
+    P=P'*P;
   case {'square'}
     P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
-    P=P'&P;
+    P=P'*P;
   case {'stripe'}
     P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
 end
