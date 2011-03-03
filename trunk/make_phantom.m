d0ed5452b395e404d6064068998edad565fce2f5
beckchang88@gmail.com <beckchang88@gmail.com@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-03 13:11:56 +0000

    case circle added, dot becomes a small circle

--- trunk/make_phantom.m
+++ trunk/make_phantom.m
@@ -13,20 +13,21 @@ function P = make_phantom(shape,N)
 
     % T width of the square pulse or the stripe. T must be an even number.
     T=round(N/4)*2;
-
+    R=T/2;
 
 switch shape
   case {'Shepp-Logan','Modified Shepp-Logan'}
     % Modified Shepp-Logan' gives better visual perception than 'Shepp-Logan'
     P = phantom(shape,N);
   case {'dot'}
-    T=2;
-    P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
-    P=P'*P;
+    R=4;
+    I=1:N; x=I-N/2; y=N/2-I; [X, Y]=meshgrid(x,y); P=(X.^2 +Y.^2 <= R^2)
   case {'square'}
     P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
     P=P'*P;
   case {'stripe'}
     P=[zeros(N,(N-T)/2) ones(N,T) zeros(N,(N-T)/2)];
+  case {'circle'}
+    I=1:N; x=I-N/2; y=N/2-I; [X, Y]=meshgrid(x,y); P=(X.^2 +Y.^2 <= R^2);
 end
 
