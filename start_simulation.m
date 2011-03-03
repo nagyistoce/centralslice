3231d5e88adb3c7796432bf2b8555132b4475ff9
beckchang88@gmail.com <beckchang88@gmail.com@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-03 12:03:21 +0000

    global deleted

--- start_simulation.m
+++ start_simulation.m
@@ -6,19 +6,19 @@
 
 %! Debug the program.
 % When DEBUG=1, extra figures will be saved in current directory which can be used for debugging process.
-global DEBUG = 1;
+DEBUG = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parameters
 
 %! Shape of the phantom.
-global shape='dot';
+shape='dot';
 
 %! Size of the phantom.
 % This specifies the number of rows and columns in the matrix of Phantom.
-global N = 80;
+N = 80;
 
 %! Signal to noise ratio.
-global SNR = 0;
+SNR = 0;
 
 main(shape,N,SNR,DEBUG);
