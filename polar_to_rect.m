c8c47cec02f073e192956c5a9f5c39add6319036
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-05 10:12:29 +0000

    Add some code to the file 'polar_to_rect.m'

--- polar_to_rect.m
+++ polar_to_rect.m
@@ -4,11 +4,18 @@
 %
 
 %%
-%! @param Fourier_Radon Matrix of Fourier transformed Radon image
-% @param THETA angles of Radon transform. Values of theta in each columns of Fourier_Radon
+%! 
+% @param theta angles of Radon transform. Values of theta in each columns of Fourier_Radon
 % @param axis_omega_s values of omega_s in each rows of Fourier_Radon
+% @param Fourier_Radon Matrix of Fourier transformed Radon image
+% @param N_image minimium size of the image
 % @param DEBUG Debug mode. If DEBUG=1, surface of Fourier_Radon in polar coordinates and in rectangular coordinates will be saved.
 % @retval Fourier_2D Matrix of the mapped Fourier space. By central slice theorem, this is equivalent to the 2D Fourier transform of the original image.
 % @retval axis_omega_xy values of omega_x (or omega_y) in the columns (or rows) of Fourier_2D.
-function [Fourier_2D axis_omega_xy] = polar_to_rect(Fourier_Radon,THETA,axis_omega_s,DEBUG)
+function [Fourier_2D axis_omega_xy] = polar_to_rect(theta,axis_omega_s,Fourier_Radon,DEBUG)
 
+% Label each elements in the matrix Fourier_Radon with the corresponding theta and omega_s:
+[THETA OMEGA_S] = meshgrid(theta,omega_s);
+
+%Define the desired scale of the rectangular coordinates
+% [OMEGA_X OMEGA_Y] = meshgrid(...);
