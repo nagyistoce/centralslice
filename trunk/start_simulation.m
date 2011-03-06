4e58ba7160d2b016e6d7673b95970b7d84dc40eb
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-06 11:26:17 +0000

    Change phantom to back to Modified Shepp-Logan

--- trunk/start_simulation.m
+++ trunk/start_simulation.m
@@ -14,15 +14,15 @@ DEBUG = 1;
 %% Parameters
 
 %! Shape of the phantom. Can be 'Shepp-Logan', 'Modified Shepp-Logan', 'dot', 'square', 'stripe' or 'offcentre dot'.
-shape='offcentre dot'
+shape='Modified Shepp-Logan'
 
 %! Size of the phantom.
 % This specifies the number of rows and columns in the matrix of Phantom.
 % N_image is suggested to be an odd number.
-N_image = 231
+N_image = 361
 
 %! Number of slices in Radon scan from 0deg to 180deg (excluding 180deg)
-N_theta = 180
+N_theta = 180*2
 
 %! Signal to noise ratio.
 SNR = 0
@@ -31,7 +31,7 @@ SNR = 0
 interp_m = 'linear'
 
 %! Oversampling_ratio oversampling ratio. Increase the Nyquist frequency to reduce aliasing. =1, none; >1 oversampling.
-oversampling_ratio=1
+oversampling_ratio=2
 
 %! Zeropadding ratio. Avoid overlapping of artefacts to the phantom after applying inverse Fourier transform. 1 ~ mininal; >1 ~ zeropadding.
 %zeropadding_ratio=1;
