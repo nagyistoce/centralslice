d7ec582dc6db4005e67f88bb8583d1f202bd5e69
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-06 09:55:51 +0000

    Change phantom to offcentre 2d unit impulse function to tackle with mirror ghost in reconstructed image

--- trunk/start_simulation.m
+++ trunk/start_simulation.m
@@ -8,21 +8,21 @@
 
 %! Debug the program.
 % When DEBUG=1, extra figures will be saved in current directory which can be used for debugging process.
-DEBUG = 0;
+DEBUG = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parameters
 
-%! Shape of the phantom. Can be 'Shepp-Logan', 'Modified Shepp-Logan', 'dot', 'square', or 'stripe'.
-shape='Modified Shepp-Logan'
+%! Shape of the phantom. Can be 'Shepp-Logan', 'Modified Shepp-Logan', 'dot', 'square', 'stripe' or 'offcentre dot'.
+shape='offcentre dot'
 
 %! Size of the phantom.
 % This specifies the number of rows and columns in the matrix of Phantom.
 % N_image is suggested to be an odd number.
-N_image = 480
+N_image = 231
 
 %! Number of slices in Radon scan from 0deg to 180deg (excluding 180deg)
-N_theta = 180*2
+N_theta = 180
 
 %! Signal to noise ratio.
 SNR = 0
@@ -31,7 +31,7 @@ SNR = 0
 interp_m = 'linear'
 
 %! Oversampling_ratio oversampling ratio. Increase the Nyquist frequency to reduce aliasing. =1, none; >1 oversampling.
-oversampling_ratio=2
+oversampling_ratio=1
 
 %! Zeropadding ratio. Avoid overlapping of artefacts to the phantom after applying inverse Fourier transform. 1 ~ mininal; >1 ~ zeropadding.
 %zeropadding_ratio=1;
