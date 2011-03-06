37bfbc61beafad859a605e1f5bc1c500013fa097
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-06 04:47:25 +0000

    Change phantom to Modified Shepp-Logan

--- trunk/start_simulation.m
+++ trunk/start_simulation.m
@@ -14,20 +14,20 @@ DEBUG = 1;
 %% Parameters
 
 %! Shape of the phantom. Can be 'Shepp-Logan', 'Modified Shepp-Logan', 'dot', 'square', or 'stripe'.
-shape='dot';
+shape='Modified Shepp-Logan'
 
 %! Size of the phantom.
 % This specifies the number of rows and columns in the matrix of Phantom.
 % N_image is suggested to be an odd number.
-N_image = 231;
+N_image = 231
 
 %! Number of slices in Radon scan from 0deg to 180deg (excluding 180deg)
-N_theta = 180;
+N_theta = 180
 
 %! Signal to noise ratio.
-SNR = 0;
+SNR = 0
 
 %! Interpolation method
-interp_m = 'nearest';
+interp_m = 'nearest'
 
 main(shape,N_image,N_theta,SNR,interp_m,DEBUG);
