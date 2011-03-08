f0ce0114832cbdf8d0fd5f7280575fb3346b6e9b
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-08 11:36:38 +0000

    Update code according to Lewis' contribution in Issue 4

---
+++ damage_sensors.m
@@ -0,0 +1,8 @@
+function damage_radon = damage_sensors(Radon, damage_ratio)
+
+damage_radon = Radon;  %copy the Radon image
+[numb_sensor scan_angle] = size(Radon); %find the size of the Radon image
+total_damage = round((numb_sensor - 1)*damage_ratio) + 1; %total number of sensor damage
+
+sensor_index = round(1 + (numb_sensor-1).*rand(1, total_damage)); 
+damage_radon(sensor_index,:) = 0; %%specify which sensors need to set to be zero
