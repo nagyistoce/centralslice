89b8bcc9d73f530f237cb20549e1ee16e6cb8d1b
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-08 15:16:58 +0000

    Add comments to the function

--- add_noise.m
+++ add_noise.m
@@ -3,7 +3,7 @@
 % Add noise to the image.
 %
 
-%! Add gaussian noise to the image. Assumes the exposure is long enougth that central limit theorem is valid.
+%! Add gaussian noise to the image. Assumes the exposure is long enough that central limit theorem is valid. (Short exposure is currently not supported)
 % 
 % Signal-to-noise ratio is estimated by the ratio ( @f$\frac{\mu}{\sigma} @f$ ), where @f$ \mu @f$ is the expected (perfect) measurement result, @f$ \sigma @f$ is the variance of the noise. In dB scale it is @f$ 20log_{10}(\frac{A_\textrm{noise}}{A_\textrm{noise}}) @f$ 
 %
