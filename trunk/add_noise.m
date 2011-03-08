4b37b3b4b7704d1c92947b5a1581348378bda731
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-08 15:15:42 +0000

    Change signal noise mode from linear to Gaussian

--- trunk/add_noise.m
+++ trunk/add_noise.m
@@ -3,18 +3,23 @@
 % Add noise to the image.
 %
 
-%! Add noise to the image.
+%! Add gaussian noise to the image. Assumes the exposure is long enougth that central limit theorem is valid.
+% 
+% Signal-to-noise ratio is estimated by the ratio ( @f$\frac{\mu}{\sigma} @f$ ), where @f$ \mu @f$ is the expected (perfect) measurement result, @f$ \sigma @f$ is the variance of the noise. In dB scale it is @f$ 20log_{10}(\frac{A_\textrm{noise}}{A_\textrm{noise}}) @f$ 
 %
 % @param image matrix of the image
-% @param SNR signal to noise ratio (1~inf)
+% @param SNRdB signal to noise ratio (1~inf)
 % @retval new_image new image loaded with noise
 
-function new_image = add_noise(image,SNR)
+function new_image = add_noise(image,SNRdB)
 
 [height width] = size(image);
 
-% support linear noise only.
-noise = (rand(height,width) *2 -1)/SNR;
+% estimate the variance of noise in each measurement of each sensor.
+SNR = 10^(SNRdB/20);
+sigma = image / SNR;
 
-new_image = image .* (1 + noise);
+noise = randn(height,width) .* sigma;
+
+new_image = image + noise;
 
