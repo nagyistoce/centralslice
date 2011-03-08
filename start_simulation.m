3088f9ed88a762b068ccc9469adb8fc6540eab69
antony.sigma <antony.sigma@2eb154be-6e00-3881-4127-23b1744a24e0>
2011-03-08 15:51:50 +0000

    Add comments to Matlab scripts/functions; update images.

--- start_simulation.m
+++ start_simulation.m
@@ -8,7 +8,7 @@
 
 %! Debug the program.
 % When DEBUG=1, extra figures will be saved in current directory which can be used for debugging process.
-DEBUG = 0;
+DEBUG = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parameters
@@ -19,24 +19,24 @@ shape='Modified Shepp-Logan'
 %! Size of the phantom.
 % This specifies the number of rows and columns in the matrix of Phantom.
 % N_image is suggested to be an odd number.
-N_image = 256
+N_image = 361
 
 %! Number of slices in Radon scan from 0deg to 180deg (excluding 180deg)
-N_theta = 180
+N_theta = 180*4
 
 %number of sensors damaged, damage_ratio varies from 0 to 1
-damage_ratio = 2/256
+damage_ratio = 0
 
-%! Signal to noise ratio.
-SNR = 0
+%! Signal to noise ratio, in deciBell (dB)
+SNRdB = inf
 
 %! Interpolation method
 interp_m = 'linear'
 
 %! Oversampling_ratio oversampling ratio. Increase the Nyquist frequency to reduce aliasing. =1, none; >1 oversampling.
-oversampling_ratio=2
+oversampling_ratio=4
 
 %! Zeropadding ratio. Avoid overlapping of artefacts to the phantom after applying inverse Fourier transform. 1 ~ mininal; >1 ~ zeropadding.
 %zeropadding_ratio=1;
 
-main(shape,N_image,N_theta,SNR,interp_m,oversampling_ratio,damage_ratio,DEBUG);
+main(shape,N_image,N_theta,SNRdB,interp_m,oversampling_ratio,damage_ratio,DEBUG);
